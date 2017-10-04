be22f55154b1e8cd2d156c031e4a6350a53ff3b5
Peter Cruckshank <petecruckshank@gmail.com>
2017-10-03 21:43:11 -0400

    tried to get the data from Wiki to show in the card title. Also made input have autofocus

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function processResult(apiResult){
  for (var i = 0; i < apiResult.query.search.length; i++){
     //   $('#display-result').append('<h3>'+apiResult.query.search[i].title+'</h3>');
     //   $('#display-result').append('<p>'+apiResult.query.search[i].snippet+'</p>');
-    $('#display-result').append( '<div class="card" style="width: 80%;"> \
+    $('#display-result').append( '<div class="card mb-3" style="width: 80%;"> \
       <div class="card-body"> \
-        <h4 class="card-title">Card title</h4> \
+        <h4 class="card-title"> Some Text </h4> \
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p> \
         <a href="#" class="btn btn-primary">Go somewhere</a> \
       </div> \
