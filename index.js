80de84cc10631c18f0e44d3b1027a21566e0dfeb
Peter Cruckshank <petecruckshank@gmail.com>
2017-10-03 21:11:46 -0400

    took out the image tag from the cards

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function processResult(apiResult){
  for (var i = 0; i < apiResult.query.search.length; i++){
     //   $('#display-result').append('<h3>'+apiResult.query.search[i].title+'</h3>');
     //   $('#display-result').append('<p>'+apiResult.query.search[i].snippet+'</p>');
-    $('#display-result').append( '<div class="card" style="width: 20rem;"> \
-      <img class="card-img-top" src="..." alt="Card image cap"> \
+    $('#display-result').append( '<div class="card" style="width: 80%;"> \
       <div class="card-body"> \
         <h4 class="card-title">Card title</h4> \
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p> \
