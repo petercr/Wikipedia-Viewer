d6379120dffbbac575fb5dc27e88c41416ffcb85
Peter Cruckshank <petecruckshank@gmail.com>
2017-10-03 21:10:01 -0400

    added bootstrap cards for styling the data

--- index.js
+++ index.js
@@ -13,9 +13,17 @@ $(document).ready(function(){
 function processResult(apiResult){
   console.log(apiResult);
  for (var i = 0; i < apiResult.query.search.length; i++){
-      $('#display-result').append('<h3>'+apiResult.query.search[i].title+'</h3>');
-      $('#display-result').append('<p>'+apiResult.query.search[i].snippet+'</p>');
+    //   $('#display-result').append('<h3>'+apiResult.query.search[i].title+'</h3>');
+    //   $('#display-result').append('<p>'+apiResult.query.search[i].snippet+'</p>');
+    $('#display-result').append( '<div class="card" style="width: 20rem;"> \
+      <img class="card-img-top" src="..." alt="Card image cap"> \
+      <div class="card-body"> \
+        <h4 class="card-title">Card title</h4> \
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p> \
+        <a href="#" class="btn btn-primary">Go somewhere</a> \
+      </div> \
+    </div>');
  }
-}
 
+}
 
