754cd7f95fb8d5a1aec03e18ff7b83fbc3a7511a
Peter Cruckshank <petecruckshank@gmail.com>
2018-06-22 20:44:47 -0400

    added .top-section & p tags #text, #author

--- wiki-app/src/App.js
+++ wiki-app/src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div id="quote-box">
-        
+        <div className="top-section">
+          <p id="text">Some cool quote...</p>
+          <p id="author">The one who thought it up..?!</p>
+        </div>
       </div>
     );
   }
